5aedf849ba376b6cebb7749caa55dfd862368303
chawoojin0425 <163067232+chawoojin0425@users.noreply.github.com>
2024-04-03 17:37:34 +0900

    Update do_hyeun.c

--- do_hyeun.c
+++ do_hyeun.c
@@ -1,49 +1,28 @@
-/*
 #include <stdio.h>
 int main(){
-	char n1[15];
-	char n2[15];
-	int a, b, c;
-	float s, a3;
-	
-	FILE *fp;
-	fp = fopen("sj.txt", "r");
-	fscanf(fp, "%s %s %d %d %d", n1, n2, &a, &b, &c);
-	fclose(fp);
-	
-	s = a + b + c;
-	a3 = s / 3;
-	printf("%s %s\n%d\n%d\n%d\n", n1, n2, a, b, c);
-	printf("average: ");
-	printf("%.2f", a3);
-	
-	return 0;
-}
-*/
-
-#include <stdio.h>
-int main(){
-    char n1[15];
-    char n2[15];
-    int score[3];
+    char n1[10]; //Jaechang\0
+    char n2[5]; //Shim\0
+    int score[3]; //88 98 86
     float s = 0, a3;
 
-    FILE *fp;
-    fp = fopen("sj.txt", "r");
-    fscanf(fp,"%s %s", n1, n2);
+    FILE *fp1;
+    fp1 = fopen("sj.txt","r"); //fread.c
+    fscanf(fp1,"%s %s", n1, n2); //배열일 땐 &주소 안 붙임
     for (int i = 0; i<3; i++){
-    	fscanf(fp, "%d", &score[i]);
-    	s = s + score[i];
+    	fscanf(fp1, "%d", &score[i]);
+    	s += score[i]; //s = s + score[i];축약대입연산자
 	}
 	a3 = s / 3;
-	
-    fclose(fp);
-
+    fclose(fp1);
+    
     printf("%s %s\n", n1, n2);
     for (int i = 0; i < 3; i++) {
         printf("%d\n", score[i]);
     }
-    printf("average: %.2f", a3);
+    FILE *fp2;
+    fp2 = fopen("sj1.txt","w"); //fwrite.c
+    fprintf(fp2, "average: %f", a3);
+    fclose(fp2);
     
     return 0;
 }
